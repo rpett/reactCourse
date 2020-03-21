51cb7bda22e4041d9ce4cff2e6c161a1cf7a3f6a
rpett <grandcoeus@hotmail.com>
2020-03-21 15:22:54 +0200

    unicafe exercises init

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,23 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
 
-const Hello = (props) => {
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const History = (props) => {
+  if(props.allClicks.length === 0)
+  {
+    return(
+      <div>
+          the app is used by pressing the buttons
+      </div>
+    )
+  }
+
   return (
     <div>
-      <p>Hello {props.name}, you {props.age}!</p>
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
 
 const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
   return (
     <div>
-      <h1>Gertins!</h1>
-      <Hello name="Gerog" age={1+2}/>
+      <div>
+        {left}
+        <Button onClick={handleLeftClick} text={'left'}/>
+        <Button onClick={handleRightClick} text={'right'}/>
+        {right}
+        <History allClicks={allClicks} />
+      </div>
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
